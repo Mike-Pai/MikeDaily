90f8b9e8a7597b3c58249c00aca71751e22987ca
MikePai <ye518612@gmail.com>
2020-12-14 16:02:45 +0800

    Initial Commit

---
+++ MikeDaily/MikeDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MikeDailyApp.swift
+//  MikeDaily
+//
+//  Created by 白謹瑜 on 2020/12/14.
+//
+
+import SwiftUI
+
+@main
+struct MikeDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
